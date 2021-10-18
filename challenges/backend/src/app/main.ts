9affd4a0fffae4724268870537d06e740247fe15
piyushk1095 <92210298+piyushk1095@users.noreply.github.com>
2021-10-19 00:17:49 +0200

    Update main.ts
    Check In by Piyush Kushwaha to get Auction record

--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -4,6 +4,9 @@ import {Logger} from "./services/Logger/classes/Logger";
 import {DependencyIdentifier} from "./DependencyIdentifiers";
 import {AuctionMonitorApp} from "./AuctionMonitorApp";
 
+const axios=require('axios');
+
+
 /*
  * Create the DI container.
  */
@@ -28,3 +31,20 @@ const app = container.resolve(AuctionMonitorApp);
 (async () => {
     await app.start();
 })();
+
+
+const auction=async ()=>{
+    const response=await axios.get('http://api-core-dev.caronsale.de/api/v2/auction/buyer/',{
+    headers: {
+      authtoken:'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IlRPS0VOLWJ1eWVyLWNoYWxsZW5nZUBjYXJvbnNhbGUuZGUiLCJ1c2VyVXVpZCI6IjA1NGQ0NTc3LTY5YTAtNGU0Yi04ZTVlLTk3NWJjZjhjNjJjNyIsImlhdCI6MTYzNDMwNTU0NiwiZXhwIjoxNjM0NTY0NzQ2fQ.ZtZcyhIcs0Jv_YeG0sTWbh54HWqpxuE_HidorR9YVzFI56MQbDnldTt9Dz_4SVNmgJ5yFpL4L6c7dg-qa9kCf0KeMfmntI4TlcAv-t3fEfx9rZW14FtobFFW0Nj1jk9Tbhv_wkvyeOg299Hphb-6GSGf8g7GBcDZRLz4O5JEITc',
+      userid: 'buyer-challenge@caronsale.de'
+  
+   } })
+   response.
+   console.log(response.data)
+  
+  }
+
+  auction()
+  
+  
